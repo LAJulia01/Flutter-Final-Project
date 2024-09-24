3909675a7ff1a491c63d61feff2dd042a4062ea1
LAJulia01 <laurenceabjulia@gmail.com>
2024-09-24 23:10:14 +0800

    feature.dart
    feature testing

---
+++ lib/feature.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Feature extends StatelessWidget {
+  const Feature({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('test'),
+      ),
+      
+    );
+  }
+}
