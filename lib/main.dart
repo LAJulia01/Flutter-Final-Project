aab5a23185de5288be54128869e3629988476be1
LAJulia01 <laurenceabjulia@gmail.com>
2024-09-28 20:33:14 +0800

    revised

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:module2_4_lab_exercise/socialmedia.dart';
+import 'package:module2_4_lab_exercise/views/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Socialmedia(),
+      home: const LoginScreen(),
     );
   }
 }
