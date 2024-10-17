ccea97b78f9a0c1fbbeb07eae955df5b8c73c62c
LAJulia01 <laurenceabjulia@gmail.com>
2024-10-17 08:22:19 +0800

    Edited as of 10/17

---
+++ lib/views/location_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+class GoogleMapsPage extends StatefulWidget {
+  @override
+  _GoogleMapsPageState createState() => _GoogleMapsPageState();
+}
+
+class _GoogleMapsPageState extends State<GoogleMapsPage> {
+  late GoogleMapController mapController;
+  LatLng _initialPosition = LatLng(14.5995, 120.9842); // Default location (Manila)
+  Position? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // If location services are not enabled, ask the user to enable them
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // If permissions are denied forever, inform the user
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // Get current location
+    _currentPosition = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    setState(() {
+      _initialPosition =
+          LatLng(_currentPosition!.latitude, _currentPosition!.longitude);
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Maps Location'),
+      ),
+      body: _initialPosition == null
+          ? Center(child: CircularProgressIndicator())
+          : GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _initialPosition,
+                zoom: 14.0,
+              ),
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+            ),
+    );
+  }
+}
