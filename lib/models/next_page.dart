06c9184e2a218b843173eb68e1d8b5b828811129
Macky Mar Layao <jingyang1902@gmail.com>
2024-11-05 14:37:09 +0800

    revised

--- lib/models/next_page.dart
+++ lib/models/next_page.dart
@@ -22,250 +22,254 @@ class _NextPageState extends State<NextPage> {
       appBar: AppBar(
         title: const Text('Next Page'),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'REQUIREMENTS',
-                  style: TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                    color: Color.fromARGB(255, 255, 83, 141),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const Divider(
-            height: 10,
-            color: Color.fromARGB(255, 0, 0, 0),
-            indent: 80,
-            endIndent: 80,
-            thickness: 2,
-          ),
-          const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          Column(
             children: [
-              Text(
-                'NANNY CARE',
-                style: TextStyle(
-                  fontSize: 17,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 255, 83, 141),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'REQUIREMENTS',
+                      style: TextStyle(
+                        fontSize: 17,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 255, 83, 141),
+                      ),
+                    ),
+                  ],
                 ),
               ),
+              const Divider(
+                height: 10,
+                color: Color.fromARGB(255, 0, 0, 0),
+                indent: 80,
+                endIndent: 80,
+                thickness: 2,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'NANNY CARE',
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 255, 83, 141),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 34),
+                child: const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Upload Profile Photos',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                constraints: const BoxConstraints(maxWidth: 350),
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.center,
+                child: GestureDetector(
+                    onTap: () async {
+                      FilePickerResult? result = await FilePicker.platform
+                          .pickFiles(
+                              allowMultiple: true,
+                              type: FileType.custom,
+                              allowedExtensions: ['pdf', 'mp4']);
+                      if (result == null) return;
+                      openFiles(result.files);
+
+                      //ope sigle file
+                      final file = result.files.first;
+                      openFile(file);
+
+                      print('Name: ${file.name}');
+                      print('bytes: ${file.name}');
+                      print('Name: ${file.size}');
+                      print('Extension: ${file.extension}');
+                      print('Path: ${file.path}');
+
+                      final newFile = await saveFilePermently(file);
+
+                      print('From Path: ${file.path!}');
+                      print('To Path: ${newFile.path}');
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: width.width,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 213, 213, 213),
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'attach file',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.upload_file,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 34),
+                child: const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Certifications',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                constraints: const BoxConstraints(maxWidth: 350),
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.center,
+                child: GestureDetector(
+                    onTap: () async {
+                      FilePickerResult? result = await FilePicker.platform
+                          .pickFiles(
+                              allowMultiple: true,
+                              type: FileType.custom,
+                              allowedExtensions: ['pdf', 'mp4']);
+                      if (result == null) return;
+                      openFiles(result.files);
+
+                      //ope sigle file
+                      final file = result.files.first;
+                      openFile(file);
+
+                      print('Name: ${file.name}');
+                      print('bytes: ${file.name}');
+                      print('Name: ${file.size}');
+                      print('Extension: ${file.extension}');
+                      print('Path: ${file.path}');
+
+                      final newFile = await saveFilePermently(file);
+
+                      print('From Path: ${file.path!}');
+                      print('To Path: ${newFile.path}');
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: width.width,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 213, 213, 213),
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'attach file',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.upload_file,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 34),
+                child: const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Valid IDs',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ),
+              Container(
+                constraints: const BoxConstraints(maxWidth: 350),
+                padding: const EdgeInsets.all(20),
+                alignment: Alignment.center,
+                child: GestureDetector(
+                    onTap: () async {
+                      FilePickerResult? result = await FilePicker.platform
+                          .pickFiles(
+                              allowMultiple: true,
+                              type: FileType.custom,
+                              allowedExtensions: ['pdf', 'mp4']);
+                      if (result == null) return;
+                      openFiles(result.files);
+
+                      //ope sigle file
+                      final file = result.files.first;
+                      openFile(file);
+
+                      print('Name: ${file.name}');
+                      print('bytes: ${file.name}');
+                      print('Name: ${file.size}');
+                      print('Extension: ${file.extension}');
+                      print('Path: ${file.path}');
+
+                      final newFile = await saveFilePermently(file);
+
+                      print('From Path: ${file.path!}');
+                      print('To Path: ${newFile.path}');
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: width.width,
+                      height: 120,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey),
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 213, 213, 213),
+                      ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'attach file',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(
+                            Icons.upload_file,
+                            size: 30,
+                          ),
+                        ],
+                      ),
+                    )),
+              ),
+              Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 130),
+                  child: customBtn('Sumbit', () {})),
             ],
           ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 34),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Upload Profile Photos',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-            ),
-          ),
-          Container(
-            constraints: const BoxConstraints(maxWidth: 350),
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.center,
-            child: GestureDetector(
-                onTap: () async {
-                  FilePickerResult? result = await FilePicker.platform
-                      .pickFiles(
-                          allowMultiple: true,
-                          type: FileType.custom,
-                          allowedExtensions: ['pdf', 'mp4']);
-                  if (result == null) return;
-                  openFiles(result.files);
-
-                  //ope sigle file
-                  final file = result.files.first;
-                  openFile(file);
-
-                  print('Name: ${file.name}');
-                  print('bytes: ${file.name}');
-                  print('Name: ${file.size}');
-                  print('Extension: ${file.extension}');
-                  print('Path: ${file.path}');
-
-                  final newFile = await saveFilePermently(file);
-
-                  print('From Path: ${file.path!}');
-                  print('To Path: ${newFile.path}');
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  width: width.width,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 213, 213, 213),
-                  ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'attach file',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Icon(
-                        Icons.upload_file,
-                        size: 30,
-                      ),
-                    ],
-                  ),
-                )),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 34),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Certifications',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-            ),
-          ),
-          Container(
-            constraints: const BoxConstraints(maxWidth: 350),
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.center,
-            child: GestureDetector(
-                onTap: () async {
-                  FilePickerResult? result = await FilePicker.platform
-                      .pickFiles(
-                          allowMultiple: true,
-                          type: FileType.custom,
-                          allowedExtensions: ['pdf', 'mp4']);
-                  if (result == null) return;
-                  openFiles(result.files);
-
-                  //ope sigle file
-                  final file = result.files.first;
-                  openFile(file);
-
-                  print('Name: ${file.name}');
-                  print('bytes: ${file.name}');
-                  print('Name: ${file.size}');
-                  print('Extension: ${file.extension}');
-                  print('Path: ${file.path}');
-
-                  final newFile = await saveFilePermently(file);
-
-                  print('From Path: ${file.path!}');
-                  print('To Path: ${newFile.path}');
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  width: width.width,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 213, 213, 213),
-                  ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'attach file',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Icon(
-                        Icons.upload_file,
-                        size: 30,
-                      ),
-                    ],
-                  ),
-                )),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 34),
-            child: const Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'Valid IDs',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-            ),
-          ),
-          Container(
-            constraints: const BoxConstraints(maxWidth: 350),
-            padding: const EdgeInsets.all(20),
-            alignment: Alignment.center,
-            child: GestureDetector(
-                onTap: () async {
-                  FilePickerResult? result = await FilePicker.platform
-                      .pickFiles(
-                          allowMultiple: true,
-                          type: FileType.custom,
-                          allowedExtensions: ['pdf', 'mp4']);
-                  if (result == null) return;
-                  openFiles(result.files);
-
-                  //ope sigle file
-                  final file = result.files.first;
-                  openFile(file);
-
-                  print('Name: ${file.name}');
-                  print('bytes: ${file.name}');
-                  print('Name: ${file.size}');
-                  print('Extension: ${file.extension}');
-                  print('Path: ${file.path}');
-
-                  final newFile = await saveFilePermently(file);
-
-                  print('From Path: ${file.path!}');
-                  print('To Path: ${newFile.path}');
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  width: width.width,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 213, 213, 213),
-                  ),
-                  child: const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'attach file',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Icon(
-                        Icons.upload_file,
-                        size: 30,
-                      ),
-                    ],
-                  ),
-                )),
-          ),
-          Container(
-              margin: const EdgeInsets.symmetric(horizontal: 130),
-              child: customBtn('Sumbit', () {})),
         ],
       ),
     );
